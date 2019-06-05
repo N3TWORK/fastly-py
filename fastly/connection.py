2aaa511b131544bab418358ed56a6307d20cd46e
Nico Huerta <nico@n3twork.com>
2019-06-05 16:14:32 -0400

    Revert "Adding debug log"
    This reverts commit 9a4a0731a42d8acd43994abc2501f262c43c183d.

--- fastly/connection.py
+++ fastly/connection.py
@@ -42,8 +42,6 @@ class Connection(object):
         if self.authenticator:
             self.authenticator.add_auth(headers)
 
-        print "Sending request: %s %s" % (method, self.root + path)
-
         self.http_conn.request(method, self.root + path, body, headers=headers)
         response = self.http_conn.getresponse()
         body = response.read()
